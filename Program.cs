6c3903fac7276dd99afa2f387c7216e0f3346278
roman.barbarossa <roman.barbarossa@iMac.local>
2023-01-13 13:08:55 +0300

    first task is ready

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Wordsld!");
+﻿
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. 
+// 
+// ____________method__________
+int degree (int num1, int num2)
+{
+    int result = num1;
+    int count = 2;
+    while (count <= num2)
+    {
+        result = result * num1;
+        count++;
+    }
+    return result;
+}
+// _________________________________
+
+System.Console.Write("Enter the number  ");
+int digit = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of degree  ");
+int deg = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Число {digit} в степени {deg} = {degree(digit, deg)}");
